d110df56813c904a9ef8848db22a66c1894c01d7
haxul <s.s.starodubov@yandex.ru>
2020-05-05 12:42:41 +0400

    add spring security to zuul

--- users/src/main/java/com/users/ws/controllers/UserController.java
+++ users/src/main/java/com/users/ws/controllers/UserController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.*;
 import javax.validation.Valid;
 
 @RestController
-@RequestMapping("/users")
+@RequestMapping("/")
 @RequiredArgsConstructor
 public class UserController {
 
