571d91f6a4a13444e18f47a3fe457e03ca42b49f
haxul <s.s.starodubov@yandex.ru>
2020-05-05 15:04:16 +0400

    fix WebSecureConfig in zuul

--- zuul/src/main/java/com/zuul/configurations/WebSecureConfig.java
+++ zuul/src/main/java/com/zuul/configurations/WebSecureConfig.java
@@ -1,6 +1,9 @@
 package com.zuul.configurations;
 
+import com.zuul.filters.AuthorizationFilter;
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -9,14 +12,18 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 
 @EnableWebSecurity
 @Configuration
+@RequiredArgsConstructor
 public class WebSecureConfig extends WebSecurityConfigurerAdapter {
+
+    private final Environment environment;
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.csrf().disable();
         http.headers().frameOptions().disable();
         http.authorizeRequests().antMatchers(HttpMethod.POST, "/users-ws/login").permitAll().
-        antMatchers(HttpMethod.POST, "/users-ws/register").permitAll().anyRequest().authenticated();
+        antMatchers(HttpMethod.POST, "/users-ws/register").permitAll().anyRequest().authenticated().
+        and().addFilter(new AuthorizationFilter(authenticationManager(), environment));
         http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
     }
 }
